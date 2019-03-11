5c8999abf2772c12ffc84d8e6c043c8a2af8fcfb
p4radoks <1140606063@nku.edu.tr>
2019-03-11 15:39:39 +0300

    stringler 1

--- degiskenler.py
+++ degiskenler.py
@@ -1,4 +1,5 @@
 #Not Hesaplama
+
 a = input("Vize Girin: ")
 vize = int(a)                   #a değikenin ekrandan aldığı değer başlangıçta string. Onu inte çevirmek için int(b) kullanıyoruz.
 b = input ("Final Girin: ")
@@ -13,3 +14,11 @@ print (sonuc)
 c = 8
 d= str(c)           #c değişkeninin aldğı değer int. string ifadeye dönüştürmek için str(c) kullanıyoruz.
 print(d)
+
+
+e = input("sayiyi girin: ")     #bu şekilde print içerisinde de tip değiştirme yapılabilir. Ekstra atama yapmamıza gerek kalmaz böylece
+f = input("sayiyi girin: ")
+print (int(e) + int(f))
+
+
+
---
+++ stringler.py
@@ -0,0 +1,11 @@
+
+print ("Ahmet")   #İçeride tırnak kullanımı olmadığı sürece tek ve çift tırnak arasında fark yok
+print ('Ahmet')
+print ("Ahmet'in amcası")    #Eğer içeride tek tırnak kullanılacaksa dışarıda çift tırnak kullanılmalı. Dışarıda tek tırnak kullanılırsa çalışmaz.
+print ("Ahmet bana dedi ki: \"Ben de Ahmet'le gelmek istiyorum\"")   # \ işareti kendinden sonra gelen işaretin aslında varolan fonksiyonda çalışmadığını belirtir.
+
+a = "Ahmet "
+b = "KAPLICA"
+print (a+b)
+
+print ("Ahmet bana dedi ki: "Ben de gelmek istiyorum" ")   #Kodun bu hali çalışmaz!  İçeride ve dışarıda çift tırnak olmaz!
