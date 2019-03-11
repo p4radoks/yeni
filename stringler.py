a6eff3f6c6bb6651b453f76a7a161ddd06178bde
p4radoks <1140606063@nku.edu.tr>
2019-03-11 16:23:29 +0300

    stingler bitti

--- stringler.py
+++ stringler.py
@@ -4,8 +4,18 @@ print ('Ahmet')
 print ("Ahmet'in amcası")    #Eğer içeride tek tırnak kullanılacaksa dışarıda çift tırnak kullanılmalı. Dışarıda tek tırnak kullanılırsa çalışmaz.
 print ("Ahmet bana dedi ki: \"Ben de Ahmet'le gelmek istiyorum\"")   # \ işareti kendinden sonra gelen işaretin aslında varolan fonksiyonda çalışmadığını belirtir.
 
+print ("Ahmet bana dedi ki: "Ben de gelmek istiyorum" ")   #Kodun bu hali çalışmaz!  İçeride ve dışarıda çift tırnak olmaz!
+
 a = "Ahmet "
 b = "KAPLICA"
 print (a+b)
 
-print ("Ahmet bana dedi ki: "Ben de gelmek istiyorum" ")   #Kodun bu hali çalışmaz!  İçeride ve dışarıda çift tırnak olmaz!
+
+
+sayi1 = 18
+print ("Ahmet'in yaşı: " + str(sayi1)) # string bir ifade ile int değeri + diyerek aynı print içerisine yazdıramıyoruz. Bu nedenle sayi1 ifadesini string bir değere dönüştürdük.
+
+print ("Ahmet'in yaşı: " + 'sayi1') # videoda göstermesine rağmen bu şekilde çalışmıyor!!!!
+
+
+
