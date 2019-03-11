6f4819eb5620a12a90d5b48284858bd34306f363
p4radoks <1140606063@nku.edu.tr>
2019-03-11 13:39:05 +0300

    modulvefonskiyon

--- degiskenler.py
+++ degiskenler.py
@@ -7,3 +7,9 @@ final = int(b)
 sonuc = vize*0.3 + final*0.7
 
 print (sonuc)
+
+
+
+c = 8
+d= str(c)           #c değişkeninin aldğı değer int. string ifadeye dönüştürmek için str(c) kullanıyoruz.
+print(d)
---
+++ modulvefonksiyon.py
@@ -0,0 +1,23 @@
+a = 5
+b = 7
+
+print (a**4)
+print (pow(5,4)) #pow a^4 manasına geliyor. üst alma işlemlerine kullanılan fonksiyon
+
+print (abs(-18)) #bu fonksiyon sayıyı mutlak değer içerisine alır
+
+
+
+import math
+
+print (math.floor(19.7))  #floor fonskiyonu math paketine bağlı bu yüzden önce math paketini import ettik. floor fonskiyonun kullanılması için başında paketin ismi olmalı.
+                        # floor fonskiyonu x değerinden önceki en büyük tam sayı değerini verir.
+
+print (math.sqrt(23))    # sqrt fonskiyonu sayının karekökünü alır
+print(math.sqrt(9))
+
+sayi = math.sqrt(49)    #sayi değişkenine math.sqrt fonksiyonunu atadık. print(sayi) dediğimizde değerin kareköklü halini yazdırır.
+print(sayi)
+
+
+#kullanım   modülismi.fonksiyonismi şeklindedir.
