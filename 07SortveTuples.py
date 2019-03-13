137f2fcfcad943b3d3cecf2046206d3fce129288
p4radoks <1140606063@nku.edu.tr>
2019-03-13 22:13:52 +0300

    sortvetuples

---
+++ 07SortveTuples.py
@@ -0,0 +1,18 @@
+sayi1=[8,56,25,875,224,156,1561,2441,12]
+sayi1.sort()        #Sort fonksiynu sayi1 dizisinin sayıların büyüklüğüne göre sıralanmasını sağlar
+print(sayi1)
+
+print(sorted("ahmet"))  #ahmet'in karakterlerini alfabetik sıraya göre sıralar
+
+isim1=["ahmet","mehmet","ali"]
+isim1.sort()    #İsimlerdeki karakterlerin alfabedeki yerlerinin toplamını alarak azdan çoğa doğru sıralar
+print(isim1)
+
+isim2=("selim","cafer","cengiz")
+print(isim2)
+# isim2.reverse()     #isim2 bir tuples olduğu için bunu içerisinde herhangi bir değişiklik YAPILAMAZ!
+
+
+
+
+
